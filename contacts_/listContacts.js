708cba3f3caa12550946aa4cdb1b603f59b94442
Serhii Panasiuk <regidmail.sergej@gmail.com>
2022-05-19 00:26:51 +0300

    fix

--- contacts_/listContacts.js
+++ contacts_/listContacts.js
@@ -1,10 +1,15 @@
-const fs = require('fs/promises');
-const db = require('../db');
+const fs = require("fs/promises");
+const db = require("../db");
 
 const listContacts = async () => {
-  const data = await fs.readFile(db.filePath);
-  const contacts = JSON.parse(data);
-  return contacts;
+  try {
+    const data = await fs.readFile(db.filePath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.error("there was an error:", error.message);
+    throw new Error(`there was an error: ${error.message}`);
+  }
 };
 
 module.exports = listContacts;
--- contacts_/updateContactsDB.js
+++ contacts_/updateContactsDB.js
@@ -1,8 +1,13 @@
-const fs = require('fs/promises');
-const db = require('../db');
+const fs = require("fs/promises");
+const db = require("../db");
 
-const updateContactsDB = async contacts => {
-  await fs.writeFile(db.filePath, JSON.stringify(contacts));
+const updateContactsDB = async (contacts) => {
+  try {
+    await fs.writeFile(db.filePath, JSON.stringify(contacts));
+  } catch (error) {
+    console.error("there was an error:", error.message);
+    throw new Error(`there was an error: ${error.message}`);
+  }
 };
 
 module.exports = updateContactsDB;
